80fd2dc3b9c8ea2a23548c3dd98dfc87f35a56a3
Luong Paul <luongpaul95@gmail.com>
2017-11-25 20:35:25 +0100

    [TEST] updating portfolio_test with FIX and random test

--- portfolio_test.py
+++ portfolio_test.py
@@ -14,14 +14,16 @@ import assetList
 from asset import Asset
 
 from portfolio_simulation import create_random_portfolios
+from portfolio_simulation import create_one_portfolio
 
 endPoint = 'https://dolphin.jump-technology.com:3389/api/v1'
 login = 'epita_user_9'
 password = 'dolphin16116'
 start_date = '2012-01-01'
 end_date = '2017-06-30'
-ratio_map = {'annual_volatility': '18', 'sharpe': '20', 'annual_returns': '17', 'returns': '21', 'correlation': '19'}
+ratio_map = {'annual_volatility': '18', 'sharpe': '20', 'annual_returns': '17', 'returns': '21'}
 portfolio_id = '571'
+currency = "EUR"
 
 exchange_rate_USD_EUR = 0.775430
 total_portfolio_budget = 10000000
@@ -35,16 +37,23 @@ class PortfolioTest(unittest.TestCase):
 
     # Construct nb_simulations portfolios
     list_portfolio = create_random_portfolios(list_assets, nb_simulations)
-    p = list_portfolio[0]
+    # Fix portfolio
+    fix_portfolio = create_one_portfolio()
 
     def test_portfolio_sharp(self):
+        print("TEST with random generated portfolio")
+        for p in self.list_portfolio:
+            self.put_portfolio(p)
+            self.test_val_vs_api_val(p.sharpe, p.volatility, p.returns, False)
+
+    def test_fix_portfolio(self):
+        print("TEST with fix portfolio")
         # for p in self.list_portfolio:
-        p = self.list_portfolio[0]
+        p = self.fix_portfolio
         self.put_portfolio(p)
-        self.test_sharpe_val_vs_api_share_val()
+        self.test_val_vs_api_val(p.sharpe, p.volatility, p.returns, True)
 
     def put_portfolio(self, p):
-        # /!\ IT QUANTITY AND NOT WEIGHT
         list_asset_repartition = p.list_asset_repartition
 
         asset_id_list = []
@@ -52,45 +61,35 @@ class PortfolioTest(unittest.TestCase):
         quantity_list = []
         for elt in list_asset_repartition:
             asset_id_list.append(elt.asset_id)
-            # /!\ convert weight into quantity
-            qte = self.convert_weight_to_quantity(elt.asset_id, elt.weight)
-            quantity_list.append(qte)
+            quantity_list.append(elt.quantity)
 
         asset_list = [{"asset": {"asset": a, "quantity": q}} for a, q in zip(asset_id_list, quantity_list)]
         body = {
             "label": "PORTFOLIO_USER9",
             "currency": {
-                "code": "EUR"
+                "code": currency
             },
             "type": "front",
             "values": {
-                start_date : asset_list
+                start_date: asset_list
             }
         }
-
         # print (json.dumps(body))
         url = endPoint + '/portfolio/{0}/dyn_amount_compo'.format(portfolio_id)
         requests.put(url, auth=HTTPBasicAuth(login, password), verify=False,
                      data=json.dumps(body))
 
-    def convert_weight_to_quantity(self, id, weight):
+    def test_val_vs_api_val(self, p_sharpe, p_volatility, p_returns, is_fix):
+        # p_sharpe = self.p.sharpe
 
-        asset = self.asset_hash_map[id]
-        budget = total_portfolio_budget * weight
-        # Get last  closing price
-        firstquote_date= sorted(asset.hash_map_quotes.keys())[0]
-        price = asset.hash_map_quotes[firstquote_date]
-        if asset.currency == 'USD':
-            price = price * exchange_rate_USD_EUR
-        return budget / price
-
-    def test_sharpe_val_vs_api_share_val(self):
-        p_sharpe = self.p.sharpe
 
         url = endPoint + '/ratio/invoke'
         body = {
             'ratio': [
-                ratio_map['sharpe']
+                ratio_map['sharpe'],
+                ratio_map['annual_volatility'],
+                ratio_map['annual_returns'],
+                ratio_map['returns'],
             ],
             'asset': [portfolio_id],
             'start_date': start_date,
@@ -102,8 +101,25 @@ class PortfolioTest(unittest.TestCase):
 
         data = json.loads(res.content.decode('utf-8'))
         api_sharpe = float(data[portfolio_id][ratio_map['sharpe']]['value'].replace(',', '.'))
+        api_annual_volatility = float(data[portfolio_id][ratio_map['annual_volatility']]['value'].replace(',', '.'))
+        api_annual_returns = float(data[portfolio_id][ratio_map['annual_returns']]['value'].replace(',', '.'))
+        api_returns = float(data[portfolio_id][ratio_map['returns']]['value'].replace(',', '.'))
+
+        if (is_fix):
+            print("TEST FIX portfolio: api_sharpe = {0} | our sharpe = {1}".format(api_sharpe, p_sharpe))
+            print("TEST FIX portfolio: api_annual_volatility = {0} | our volatility = {1}".format(api_annual_volatility, p_volatility))
+            print("TEST FIX portfolio: api_annual_returns = {0} | our returns = {1}".format(api_annual_returns, p_returns))
+            print("TEST FIX portfolio: api_returns = {0} | our returns = {1}".format(api_returns, p_returns))
+        else:
+            print("TEST random portfolio: api_sharpe = {0} | our sharpe = {1}".format(api_sharpe, p_sharpe))
+            print("TEST random portfolio: api_annual_volatility = {0} | our volatility = {1}".format(api_annual_volatility, p_volatility))
+            print("TEST random portfolio: api_annual_returns = {0} | our returns = {1}".format(api_annual_returns, p_returns))
+            print("TEST random portfolio: api_returns = {0} | our returns = {1}".format(api_returns, p_returns))
 
         self.assertAlmostEqual(api_sharpe, p_sharpe, delta=assert_equal_range)
+        self.assertAlmostEqual(api_annual_volatility, p_volatility, delta=assert_equal_range)
+        self.assertAlmostEqual(api_annual_returns, p_returns, delta=assert_equal_range)
+        self.assertAlmostEqual(api_returns, p_returns, delta=assert_equal_range)
 
 
 unittest.main()
